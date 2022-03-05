949ffdc660cbb8a581dc0243b325c1f6c88e28ee
Andrea <andrea@yaaaw.it>
2022-03-05 12:39:57 +0100

    updated script

--- bpenv.sh
+++ bpenv.sh
@@ -2,7 +2,25 @@
 #! /bin/bash
 
 main() {
+  
+  key=()
+  values=()
 
+  while read line; do
+  
+  # allow comments in the .env file
+  if [[ ! ${line:1:0} = "#" ]]; then
+    continue
+  fi 
+
+  done < .env
+
+  if [[ ${#key[@]} -eq ${#values[@]} ]]; then
+
+  else
+    echo "Error: Invalid .env file"
+    exit 1
+  fi
 }
 
 main
