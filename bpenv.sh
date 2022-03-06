9c518e42cbdcb5595ee19902f94ff4b763d978fb
Andrea <andrea@yaaaw.it>
2022-03-06 13:46:00 +0100

    updated script

--- bpenv.sh
+++ bpenv.sh
@@ -2,21 +2,17 @@
 #! /bin/bash
 
 main() {
-  
-  keys=()
-  values=()
 
-  while IFS= read -r line; do
+  cat .env | while read -r line || [[ -n "$line" ]]; do
 
     # allow comments in the .env file
     if [[ ${line:1:0} = "#" ]]; then
       continue
     fi 
-
-    keys+=$(cut -d = -f1 <<< $line)
-    values+=$(cut -d = -f2- <<< $line)
-
-  done < .env
+    
+    key=$(cut -d = -f1 <<< $line)
+    value=$(cut -d = -f2- <<< $line)
+  done 
 }
 
 main
